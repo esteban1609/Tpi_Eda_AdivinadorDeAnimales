5ccc396a562a101683dac238204345c27ee02360
AntonioAssat <turcotomas280302@gmail.com>
2026-06-04 20:44:14 -0300

    agregue menu principal

--- Tpi_Eda_AdivinadorDeAnimales/src/tpi_eda_adivinadordeanimales/Tpi_Eda_AdivinadorDeAnimales.java
+++ Tpi_Eda_AdivinadorDeAnimales/src/tpi_eda_adivinadordeanimales/Tpi_Eda_AdivinadorDeAnimales.java
@@ -5,6 +5,8 @@
  */
 package tpi_eda_adivinadordeanimales;
 
+import java.util.Scanner;
+
 /**
  *
  * @author esteb
@@ -16,20 +18,59 @@ public class Tpi_Eda_AdivinadorDeAnimales {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Arbol arbol = new Arbol();
+        //Arbol arbol = new Arbol();
 
-        arbol.crearArbolInicial();
+        //arbol.crearArbolInicial();
 
-        System.out.println("\nARBOL CREADO:\n");
-        arbol.mostrarArbol();
+        //System.out.println("\nARBOL CREADO:\n");
+        //arbol.mostrarArbol();
         
-        System.out.println("Comienza el juego");
-        arbol.jugar();
+        //System.out.println("Comienza el juego");
+        //arbol.jugar();
         
         //System.out.println(arbol.getRaiz().getDato());
         //System.out.println(arbol.getRaiz().getSi().getDato());
        // System.out.println(arbol.getRaiz().getNo().getDato());
-        
+        Scanner sc = new Scanner(System.in);
+
+        Arbol arbol = new Arbol();
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n=== ADIVINADOR DE ANIMALES ===");
+            System.out.println("1 - Crear árbol inicial");
+            System.out.println("2 - Mostrar árbol");
+            System.out.println("3 - Jugar");
+            System.out.println("4 - Salir");
+            System.out.print("Opción: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch(opcion) {
+
+                case 1:arbol.crearArbolInicial();
+
+                    break;
+
+                case 2:arbol.mostrarArbol();
+
+                    break;
+
+                case 3:arbol.jugar();
+
+                    break;
+
+                case 4:System.out.println("Fin del programa.");
+
+                    break;
+
+                default:System.out.println("Opción inválida.");
+            }
+
+        } while(opcion != 4);
     }
     
 }
